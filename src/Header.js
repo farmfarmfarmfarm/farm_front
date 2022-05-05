eb3b55a130e434d20ac3ffec3ec9edc022048eab
최진실 <jinsil378900@gmail.com>
2022-05-05 23:50:03 +0900

    🎨[Design] header 초안

--- src/Header.js
+++ src/Header.js
@@ -3,23 +3,14 @@ import './App.css';
 
 function Header(props) {
   return (
-    <div className='Header'>
-      <h1 class="ci"><a href="/" title="farmstory 메인으로 가기">farmstory</a></h1>
-      <div class="util-area">
-        <div class="left-link">
-          <a href="https://www.megabox.co.kr/benefit/viplounge" title="VIP LOUNGE">VIP LOUNGE</a>
-          <a href="https://www.megabox.co.kr/benefit/membership" title="멤버십">멤버십</a>
-          <a href="https://www.megabox.co.kr/support" title="고객센터">고객센터</a>
-        </div>
-        <div class="right-link">
-          <div class="before">
-            <a href="javaScript:fn_viewLoginPopup('default','pc')" title="로그인">로그인</a>
-            <a href="https://www.megabox.co.kr/join" title="회원가입">회원가입</a>
-            <a href="https://www.megabox.co.kr/booking">빠른예매</a>
-          </div>
-        </div>
+    <div id='header'>
+      <h1 className="ci"><a href="/" title="farmstory 메인으로 가기">
+        <img src="img/logo.jpg" />farmstory
+      </a></h1>
+      <div className="link-area">
+        <a href="/" className="header-open-layer btn-layer-sitemap" title="사이트맵">사이트맵</a>
+        <a href="/" className="header-open-layer btn-layer-login" title="로그인 하기">로그인</a>
       </div>
-        
     </div>
   );
 }
