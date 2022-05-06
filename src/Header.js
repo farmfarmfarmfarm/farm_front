52eb25802605d91f3fca433f6f0b61ccb9b8481a
yejinleee <yejin9487@gmail.com>
2022-05-07 01:51:08 +0900

    🚚Rename : Remove unnecessary

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header(props) {
   return (
     <div>
       <header id='header'>
-        <h1 className="ci"><a href="/" title="farmstory 메인으로 가기"><img src="img/logo.jpg" />farmstory</a></h1>
+        <h1 className="ci"><a href="/" title="farmstory 메인으로 가기"><img src="img/logo.jpg" alt="logo" />farmstory</a></h1>
         <div className="link-area">
           <a href="/" className="btn-layer-sitemap" title="사이트맵">사이트맵</a>
           <a href="/" className="btn-layer-login" title="로그인 하기">로그인</a>
