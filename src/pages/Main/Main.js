7a7e6e7fb2ae21df7233bd7120afe9193ad270c0
yejinleee <yejin9487@gmail.com>
2022-06-02 22:33:01 +0900

    🔥Feat : add NaverMap

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Check from "pages/Home/Check";
+import { Navermap } from './Navermap';
 
 function Main() {
     const [checkedItems, setcheckedItems] = useState([]);
@@ -9,6 +10,7 @@ function Main() {
             <input type="text" placeholder='어느 지역을 찾으시나요?' />
             <div>어떤 농장을 가볼까요?</div>
             <Check checkedItems={checkedItems} setcheckedItems={setcheckedItems}></Check>
+            <Navermap />
         </div>
         
     );
