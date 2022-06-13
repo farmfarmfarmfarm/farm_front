6e755135440a8ed05d6de73a407562821267aae1
yejinleee <yejin9487@gmail.com>
2022-06-13 15:33:18 +0900

    로고 변경, 기타 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import './App.css';
 import { StHeader, StHeaderMain, StNav } from './style';
-import logo from 'assets/icons/logo.jpg';
+import logo from 'assets/icons/newlogo.jpg';
 import login from 'assets/icons/login.png';
 import {Link} from "react-router-dom";
 
@@ -9,7 +9,7 @@ function Header({isLoggedIn}) {
   return (
     <StHeader>
       <StHeaderMain>
-        <Link to="/"><img src={logo} alt="로고"></img></Link>
+        <Link to="/"><img style={{border: '1px solid black'}} src={logo} alt="로고"></img></Link>
         {isLoggedIn ? <Link to="/profile"><img src={login} alt="프로필"></img></Link>
         : <Link to="/login"><img src={login} alt="로그인"></img></Link> }
       </StHeaderMain>
