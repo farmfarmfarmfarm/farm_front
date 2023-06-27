56183d4f05fe1babf14245f7a90770ff198c0b13
yejinleee <yejin9487@gmail.com>
2023-06-28 01:06:37 +0900

    style: logo style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,55 @@
-import React from 'react';
-import { StHeader, StHeaderMain, StNav } from './style';
-import logo from 'assets/icons/newlogo.jpg';
-import login from 'assets/icons/login.png';
-import menu from 'assets/icons/nav_bar.png'
-import {Link} from "react-router-dom";
+import React from "react";
+import { StHeader, StHeaderMain, StNav } from "./style";
+import logo from "assets/icons/newlogo.jpg";
+import login from "assets/icons/login.png";
+import menu from "assets/icons/nav_bar.png";
+import { Link } from "react-router-dom";
 
-function Header({isLoggedIn}) {
+function Header({ isLoggedIn }) {
   return (
     <StHeader>
       <StHeaderMain>
-        <Link to="menu" style={{width: '60px', height:'60px'}}><img style={{width: '60px', height:'60px'}} src={menu} alt="메뉴"></img></Link>
-        <Link to="/"><img style={{width: '100px', height:'100px'}} src={logo} alt="로고"></img></Link>
-        {isLoggedIn ? <Link to="/profile" style={{width: '60px', height:'60px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><img src={login} alt="프로필"></img></Link>
-        : <Link to="/login" style={{width: '60px', height:'60px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><img style={{width: '30px', height:'30px'}} src={login} alt="로그인"></img></Link> }
+        <Link to="menu" style={{ width: "60px", height: "60px" }}>
+          <img
+            style={{ width: "60px", height: "60px" }}
+            src={menu}
+            alt="메뉴"
+          ></img>
+        </Link>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h3 className="mainlogo">Therapia</h3>
+        </Link>
+        {isLoggedIn ? (
+          <Link
+            to="/profile"
+            style={{
+              width: "60px",
+              height: "60px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src={login} alt="프로필"></img>
+          </Link>
+        ) : (
+          <Link
+            to="/login"
+            style={{
+              width: "60px",
+              height: "60px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img
+              style={{ width: "30px", height: "30px" }}
+              src={login}
+              alt="로그인"
+            ></img>
+          </Link>
+        )}
       </StHeaderMain>
     </StHeader>
   );
--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -15,104 +15,67 @@ const Home = () => {
   const [rcUserId, setRcUserId] = useRecoilState(userId);
   const [nickname, setNickname] = useState(localStorage.getItem("nickname"));
   const navigate = useNavigate();
-  // console.log(rcUserId);
+  console.log(rcUserId);
 
   return (
-    <div>
-      <div>
-        <div className="home">
-          <div className="menulogo">
-            <div className="mainlogo">대충 테라피아 로고</div>
-            {rcUserId === 0 ? (
-              <div className="before">
-                <Link to="/login" style={{ color: "black" }}>
-                  로그인 해주세요{" "}
-                  <img src={rightArrow} style={{ width: "15px" }} />
-                </Link>
-              </div>
-            ) : (
-              <div className="after">
-                안녕하세요 .{" "}
-                <h2 style={{ letterSpacing: "2px" }}>
-                  <b style={{ color: "#166d1e" }}>{nickname}</b>님
-                </h2>
-              </div>
-            )}
-            <img onClick={() => navigate(-1)} className="goback" src={close} />
+    <div className="home">
+      <div className="menulogo">
+        <div className="mainlogo">Therapia</div>
+        {rcUserId === 0 ? (
+          <div className="before">
+            <Link to="/login" style={{ color: "black" }}>
+              로그인 해주세요 <img src={rightArrow} style={{ width: "15px" }} />
+            </Link>
           </div>
-          <div className="nav">
-            <div className="navcontents">
-              <hr></hr>
-              <div className="navcontent">
-                <Link to="/careFarm" className="navtitle">
-                  <div>
-                    <img className="navlogo" src={listFarm} />
-                    치유 농장 소개
-                  </div>
-                  <img className="right-arrow" src={rightArrow} />
-                </Link>
-              </div>
-              <div className="navcontent">
-                <Link to="/home" className="navtitle">
-                  <div>
-                    <img className="navlogo" src={search} />
-                    지도에서 찾아보기
-                  </div>
-                  <img className="right-arrow" src={rightArrow} />
-                </Link>
-              </div>
-              <div className="navcontent">
-                <Link to="/care" className="navtitle">
-                  <div>
-                    <img className="navlogo" src={nav3} />
-                    작물로 치유하기
-                  </div>
-                  <img className="right-arrow" src={rightArrow} />
-                </Link>
-              </div>
-              <div className="navcontent">
-                <Link to="/allcrops" className="navtitle">
-                  <div>
-                    <img className="navlogo" src={nav4} />
-                    작물 찾아보기
-                  </div>
-                  <img className="right-arrow" src={rightArrow} />
-                </Link>
-              </div>
-            </div>
+        ) : (
+          <div className="after">
+            안녕하세요 .{" "}
+            <h2 style={{ letterSpacing: "2px" }}>
+              <b style={{ color: "#166d1e" }}>{nickname}</b>님
+            </h2>
           </div>
-        </div>
+        )}
+        <img onClick={() => navigate(-1)} className="goback" src={close} />
+      </div>
+      <div className="nav">
         <div className="navcontents">
-          <div>
+          <hr></hr>
+          <div className="navcontent">
             <Link to="/careFarm" className="navtitle">
-              <img className="navlogo" src={listFarm} />
-              치유 농장 소개
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <img className="navlogo" src={listFarm} />
+                치유 농장 소개
+              </div>
+              <img className="right-arrow" src={rightArrow} />
             </Link>
           </div>
-          <div>
+          <div className="navcontent">
             <Link to="/home" className="navtitle">
-              <img className="navlogo" src={search} />
-              지도에서 찾아보기
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <img className="navlogo" src={search} />
+                지도에서 찾아보기
+              </div>
+              <img className="right-arrow" src={rightArrow} />
             </Link>
           </div>
-          <div>
+          <div className="navcontent">
             <Link to="/care" className="navtitle">
-              <img className="navlogo" src={nav3} />
-              작물로 치유하기
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <img className="navlogo" src={nav3} />
+                작물로 치유하기
+              </div>
+              <img className="right-arrow" src={rightArrow} />
             </Link>
           </div>
-          <div>
+          <div className="navcontent">
             <Link to="/allcrops" className="navtitle">
-              <img className="navlogo" src={nav4} />
-              작물 찾아보기
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <img className="navlogo" src={nav4} />
+                작물 찾아보기
+              </div>
+              <img className="right-arrow" src={rightArrow} />
             </Link>
           </div>
-        </div>
-        <div className="arrow">
-          <div>&gt;</div>
-          <div>&gt;</div>
-          <div>&gt;</div>
-          <div>&gt;</div>
         </div>
       </div>
     </div>
